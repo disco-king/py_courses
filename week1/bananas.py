a758ac3055545cbf4b2f1faf31aab4f1d44e70eb
disco-king <acalabash@gmail.com>
2022-06-26 09:41:14 +0300

    PEP compliance

--- week1/bananas.py
+++ week1/bananas.py
@@ -14,19 +14,23 @@ def bananas(s) -> set:
                 if pos < 6 and i == item[pos]:
                     pos += 1
                 else:
-                    buff.insert(i, '-')
-            result.add(''.join(buff))
+                    buff.insert(i, "-")
+            result.add("".join(buff))
     return result
 
 
-# print(bananas("bbananana"))
+#if __name__ == "__main__":
+    print(bananas("bbananana"))
 
-# assert bananas("banann") == set()
-# assert bananas("banana") == {"banana"}
-# assert bananas("bbananana") == {
-#     "b-an--ana", "-banana--", "-b--anana", "b-a--nana", "-banan--a",
-#     "b-ana--na", "b---anana", "-bana--na", "-ba--nana", "b-anan--a",
-#     "-ban--ana", "b-anana--"
-#     }
-# assert bananas("bananaaa") == {"banan-a-", "banana--", "banan--a"}
-# assert bananas("bananana") == {"ban--ana", "ba--nana", "bana--na", "b--anana", "banana--", "banan--a"}
+    assert bananas("banann") == set()
+    assert bananas("banana") == {"banana"}
+    assert bananas("bbananana") == {
+        "b-an--ana", "-banana--", "-b--anana", "b-a--nana", "-banan--a",
+        "b-ana--na", "b---anana", "-bana--na", "-ba--nana", "b-anan--a",
+        "-ban--ana", "b-anana--"
+        }
+    assert bananas("bananaaa") == {"banan-a-", "banana--", "banan--a"}
+    assert bananas("bananana") == {
+        "ban--ana", "ba--nana", "bana--na",
+        "b--anana", "banana--", "banan--a"
+        }
--- week1/count_find_num.py
+++ week1/count_find_num.py
@@ -20,22 +20,27 @@ def count_find_num(primesL, limit):
         places += 1
     return [len(ret), max(ret)] if ret else []
 
-# primesL = [2, 3]
-# limit = 200
-# assert count_find_num(primesL, limit) == [13, 192]
 
-# primesL = [2, 5]
-# limit = 200
-# assert count_find_num(primesL, limit) == [8, 200]
+if __name__ == "__main__":
+    print(count_find_num([127, 137, 337], 25923620897089))
 
-# primesL = [2, 3, 5]
-# limit = 500
-# assert count_find_num(primesL, limit) == [12, 480]
+    primesL = [2, 3]
+    limit = 200
+    assert count_find_num(primesL, limit) == [13, 192]
 
-# primesL = [2, 3, 5]
-# limit = 1000
-# assert count_find_num(primesL, limit) == [19, 960]
+    primesL = [2, 5]
+    limit = 200
+    assert count_find_num(primesL, limit) == [8, 200]
 
-# primesL = [2, 3, 47]
-# limit = 200
-# assert count_find_num(primesL, limit) == []
+    primesL = [2, 3, 5]
+    limit = 500
+    assert count_find_num(primesL, limit) == [12, 480]
+
+    primesL = [2, 3, 5]
+    limit = 1000
+    assert count_find_num(primesL, limit) == [19, 960]
+
+    primesL = [2, 3, 47]
+    limit = 200
+    assert count_find_num(primesL, limit) == []
+
