13ddac4a3de6274eefee6d75b10b965908193df0
disco-king <acalabash@gmail.com>
2022-06-26 13:41:41 +0300

    additional sanity checks

--- week1/count_find_num.py
+++ week1/count_find_num.py
@@ -1,8 +1,11 @@
 from math import prod
 from itertools import combinations_with_replacement as combs
 
+
 def count_find_num(primesL, limit):
     ret = []
+    if not primesL or limit < 0:
+        return ret
     step = prod(primesL)
     next_step = True
     places = 1
@@ -22,6 +25,9 @@ def count_find_num(primesL, limit):
 
 
 if __name__ == "__main__":
+    print(count_find_num([], 100))
+    print(count_find_num([2, 3, 5], 0))
+    print(count_find_num([2, 3, 5], -1))
     print(count_find_num([127, 137, 337], 25923620897089))
 
     primesL = [2, 3]
@@ -43,4 +49,4 @@ if __name__ == "__main__":
     primesL = [2, 3, 47]
     limit = 200
     assert count_find_num(primesL, limit) == []
-
+    
