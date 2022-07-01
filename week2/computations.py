554f402feab184110d6c28ad95357a1ef5ae61df
disco-king <acalabash@gmail.com>
2022-07-02 00:31:12 +0300

    ending mechanism is a crutch

--- week2/computations.py
+++ week2/computations.py
@@ -1,16 +1,21 @@
 
-
 def distance(point_1, point_2) -> float:
+    """
+    Функция вычисляет расстояние между двумя точками
+    """
     return ((point_2[0] - point_1[0]) ** 2
             + (point_2[1] - point_1[1]) ** 2) ** 0.5
 
 
 def whole_run(base, points):
+    """
+    Функция вычисляет расстояние всего маршрута
+    """
     total = float()
 
     for i in range(len(points)-1):
         total += distance(points[i], points[i+1])
-    # print(f" distance without base {total}")
     total += distance(base, points[0]) + distance(base, points[-1])
+
     return total
 
--- week2/postman.py
+++ week2/postman.py
@@ -3,12 +3,6 @@ from computations import whole_run, distance
 import parsing
 import sys
 
-# points = [(1,1), (4,1), (1,5)]
-# points = [(1,1), (2,2), (3,3), (4,4)]
-# points = [(1,1), (2,2), (3,3), (4,4), (5,5)]
-# points = [(0,2), (2,5), (5,2), (6,6)]
-# points = [(0,2), (2,5), (5,2), (6,6), (8,3)]
-
 if len(sys.argv) == 1:
     parsing.error_exit(0)
 
@@ -28,21 +22,34 @@ for i in range(line_num):
 combs = permutations(points[1:], len(points) - 1)
 res = next(combs)
 min_run = whole_run(points[0], res)
-for i in combs:
-    run = whole_run(points[0], i)
+for comb in combs:
+    run = whole_run(points[0], comb)
     if run < min_run:
         min_run = run
-        res = i
+        res = comb
 
-print(f"\n {lines[points[0]]} {points[0]} пройдено 0.0 ->")
-dst = distance(points[0], res[0])
+print()
+dst = 0.0
+ending = "->"
 
-for i in range(len(res)-1):
-    print(f" {lines[res[i]]} {res[i]} пройдено {dst} ->")
-    dst += distance(res[i], res[i+1])
+for i in range(-1, len(res)+1):
+    if i == -1 or i == len(res):
+        location = lines[points[0]]
+        coordinates = points[0]
+    else:
+        location = lines[res[i]]
+        coordinates = res[i]
 
-i += 1
-print(f" {lines[res[i]]} {res[i]} пройдено {dst} ->")
-print(f" {lines[points[0]]} {points[0]}"
-        f" пройдено {dst + distance(res[i], points[0])}")
+    print(f" {location.ljust(30)}", end=" ")
+    print(str(coordinates).ljust(15), end=" ")
+    print(f"пройдено {dst} {ending}")
+
+    if i == -1:
+        dst = distance(points[0], res[0])
+    elif i == len(res)-1:
+        ending = ""
+        dst += distance(res[i], points[0])
+    elif i < len(res):
+        dst += distance(res[i], res[i+1])
+
 print(f"\n Кратчайшее возможное расстояние: {min_run}")
