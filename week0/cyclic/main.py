b89a3ca094bd825a31619edbca88c1966da9fa5e
disco-king <acalabash@gmail.com>
2022-07-10 12:00:54 +0300

    schedule in the form of function

--- week0/cyclic/main.py
+++ week0/cyclic/main.py
@@ -1,7 +1,7 @@
 from cyclic import CyclicIterator
 import sys
 
-lst = [i for i in range(1000000)]
+lst = (i for i in range(1000000))
 print(sys.getsizeof(lst))
 
 it = CyclicIterator(lst)
---
+++ week0/schedule/schedule.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+from typing import Generator, List, Tuple
+
+
+@dataclass
+class Movie:
+    title: str
+    dates: List[Tuple[datetime, datetime]]
+
+    def schedule(self) -> Generator[datetime, None, None]:
+        day_increment = timedelta(days=1)
+        for date in self.dates:
+            ptr = date[0]
+            while ptr <= date[1]:
+                yield ptr
+                ptr += day_increment
+
+
+m = Movie('sw', [
+  (datetime(2020, 1, 1), datetime(2020, 1, 7)),
+  (datetime(2020, 1, 15), datetime(2020, 2, 7))
+])
+
+for d in m.schedule():
+    print(d)
