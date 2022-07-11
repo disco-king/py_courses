ff2e677211d09b4fca77e83d188321492ffc27de
disco-king <acalabash@gmail.com>
2022-07-11 12:48:21 +0300

    repeat code format and tests

--- week3/cyclic/classes.py
+++ week3/cyclic/classes.py
@@ -1,19 +1,28 @@
+
 class Range:
 
     def __init__(self, limit: int):
+        """ Инициализация атрибутов класса. """
         self.count = 0
         self.state = 0
         self.limit = limit
 
     def __iter__(self):
+        """ Создание и возврат класса-итератора. """
         return Iterator(self)
 
 class Iterator:
 
     def __init__(self, obj):
+        """ Инициализация атрибута класса. """
         self.obj = obj
 
     def __next__(self):
+        """
+        Переменная state инкрементируется
+        и возвращается до тех пор, пока
+        не станет больше лимита или равна ему.
+        """
         if self.obj.state >= self.obj.limit:
             raise StopIteration
         ret = self.obj.state
@@ -23,13 +32,24 @@ class Iterator:
 
 class Range2:
     def __init__(self, stop_value: int):
+        """ Инициализация атрибутов класса. """
         self.current = -1
         self.stop_value = stop_value - 1
 
     def __iter__(self):
+        """
+        Так как класс выполняет функции
+        и итерируемого объекта, и итератора,
+        в качестве итератора он возвращает сам себя.
+        """
         return self
 
     def __next__(self):
+        """
+        Переменная state инкрементируется
+        и возвращается до тех пор, пока
+        не станет больше лимита или равна ему.
+        """
         if self.current < self.stop_value:
             self.current += 1
             return self.current
--- week3/repeat/repeat.py
+++ week3/repeat/repeat.py
@@ -4,6 +4,7 @@ class RepeatDecorator:
 
     def __init__(self, count: int, start_sleep_time: float,
                     factor: int, border_sleep_time: float):
+        """ Инициализация атрибутов параметрами init. """
         self.count = count
         if start_sleep_time > border_sleep_time:
             self.pause = border_sleep_time
@@ -13,22 +14,35 @@ class RepeatDecorator:
         self.border_time = border_sleep_time
 
     def __call__(self, func):
-
+        """
+        Магический метод, позволяющий вызвать класс как функцию.
+        """
         def wrapper(*args, **kwargs):
-            
+            """
+            Функция-обертка, выставляющая паузы для основной фунции.
+            Вызов основной функции производится через
+            экспоненциально увеличивающиеся промежутки времени.
+            При этом пауза перед первым вызовом
+            и после последнего вызова отсутствует.
+            """
             start = time.perf_counter()
             func()
             for i in range(self.count - 1):
+                print("Пауза:", self.pause)
                 time.sleep(self.pause)
                 curr = time.perf_counter()
-                print("pause:", self.pause, end = " ")
-                print("time passed:", curr - start)
+                print("Время (сек) с начала работы:", curr - start)
                 self._update_sleep_time()
                 func()
 
         return wrapper
 
     def _update_sleep_time(self):
+        """
+        Обновление времени ожидания.
+        Если время ожидания превысило лимит,
+        ему присвается значение лимита.
+        """
         if self.pause < self.border_time:
             self.pause *= 2 ** self.factor
         if self.pause > self.border_time:
@@ -40,4 +54,28 @@ def func():
     print("<<< func called >>>")
 
 if __name__ == "__main__":
+    print("5 вызовов, неограниченный рост до 8 сек.:")
     func()
+    print()
+
+    print("3 вызова, неограниченный рост до 6 сек.:")
+    @RepeatDecorator(3, 3, 1, 20)
+    def func():
+        print("<<< func called >>>")
+    func()
+    print()
+
+    print("4 вызова, рост ограничен"
+            " значением в 3 сек. на втором вызове:")
+    @RepeatDecorator(4, 2, 5, 3)
+    def func():
+        print("<<< func called >>>")
+    func()
+    print()
+
+    print("4 вызова, рост отсутствует:")
+    @RepeatDecorator(4, 1, 0, 100)
+    def func():
+        print("<<< func called >>>")
+    func()
+
