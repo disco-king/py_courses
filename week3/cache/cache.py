78bca7357a936b5d68f7884e88d3bee6aee68d14
acalabash <acalabash@gmail.com>
2022-07-11 09:56:35 +0300

    cache task code format

--- week3/cache/cache.py
+++ week3/cache/cache.py
@@ -1,15 +1,35 @@
 
 def cache_decorator(f, verbose=False):
+    """
+    Функция принимает в качестве аргумента
+    декорируемую функцию, а возвращает
+    функцию - результата декорирования.
+    Второй, опциональный аргумент - булево значение,
+    определяющее, будет ли возвращаемая функция
+    выводить диагностические сообщения.
+    """
     cache = {}
 
     def wrapper(*args, **kwargs):
+        """
+        Функция-обертка принимает произвольное
+        количество параметров, и возвращает
+        объект - результат работы декорируемой функции.
+        Если функция уже вызывалась для полученных аргументов,
+        значение возвращается из кэша, если нет -
+        вызывается функция, а результат кэшируется.
+        """
         nonlocal cache
-        hash_val = hash(str(args) + str(kwargs))
-        signal = "КЭШ"
+        # В качестве ключа для словаря используется
+        # представление всех аргументов функции
+        # в виде строки. Таким образом хэширование возможно
+        # даже для нехэшируемых типов.
+        hash_val = str(args) + str(kwargs)
+        signal = 'КЭШ'
 
         if hash_val not in cache:
             cache[hash_val] = f(*args, **kwargs)
-            signal = "ВЫЗОВ"
+            signal = 'ВЫЗОВ'
 
         if verbose:
             print(signal, end=': ')
@@ -22,24 +42,30 @@ def cache_decorator(f, verbose=False):
 def multiplier(number: int):
     return number * 2
 
+# Еще две функции чтобы убедиться,
+# что декоратор может работать с любым
+# количеством и типами аргументов.
 def adder(item):
     return item + item
-adder = cache_decorator(adder, True)
 
 def str_multiplier(string: str, number: int):
     return string * number
+
+adder = cache_decorator(adder, True)
 str_multiplier = cache_decorator(str_multiplier, True)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
-    print('\nумножаем int на 2 (пока без комментариев):')
+    print('\nУмножаем int на 2 (пока без комментариев):')
     print(1, ': ', multiplier(1), sep='')
     print(15, ': ', multiplier(15), sep='')
     print(-89, ': ', multiplier(-89), sep='')
     print(3000, ': ', multiplier(3000), sep='')
 
     multiplier = cache_decorator(multiplier, True)
-    print('\nтеперь убедимся, что кэш работает:')
+    print('\nТеперь убедимся, что кэш работает.\n'
+        'При первом вызове с аргументом X\n'
+        'функция печатает ВЫЗОВ, при повторном - КЭШ:\n')
     print(multiplier(1))
     print(multiplier(15))
     print(multiplier(1))
@@ -47,19 +73,17 @@ if __name__ == "__main__":
     print(multiplier(15))
 
 
-    print('\nработа с другими типами данных'
-            ' и комментариями для наглядности:')
+    print('\nРабота с другими типами данных:')
 
-    print('\nскладываем строки и списки сами с собой:')
-    print(adder("foo"))
-    print(adder("bar"))
-    print(adder("foo"))
-    print()
+    print('\nСкладываем строки и списки сами с собой:')
+    print(adder('foo'))
+    print(adder('bar'))
+    print(adder('foo'))
     print(adder([1, 2, 3]))
     print(adder([4, 5, 6]))
     print(adder([1, 2, 3]))
 
-    print('\nумножаем строки на число:')
-    print(str_multiplier("yo", 3))
-    print(str_multiplier("ole", 3))
-    print(str_multiplier("yo", 3))
+    print('\nУмножаем строки на число:')
+    print(str_multiplier('yo ', 3))
+    print(str_multiplier('ole ', 4))
+    print(str_multiplier('yo ', 3))
--- week3/cache/redis_cache.py
+++ week3/cache/redis_cache.py
@@ -2,22 +2,46 @@ import redis
 
 
 def cache_decorator(f, verbose=False):
-
-    r = redis.Redis()
-    if not r.ping():
-        print('нет доступа к redis')
+    """
+    Функция принимает в качестве аргумента
+    декорируемую функцию, а возвращает объект
+    функции - результата декорирования.
+    Второй, опциональный аргумент - булево значение,
+    определяющее, будет ли возвращаемая функция
+    выводить диагностические сообщения.
+    """
+    try:
+        # Пробуем подключиться к БД,
+        # проверяем подключение с помощью ping.
+        r = redis.Redis()
+        r.ping()
+    except:
+        print('Нет доступа к Redis')
         return None
 
+    # Кэшируемые данные хранятся в БД
+    # в объекте с типом данных hash.
+    # Присвоим объекту уникальное имя.
     rid = id(r)
     hash_name = f'mul_hash:{rid}'
 
-    # позже удалим хэш, чтобы не засорять БД
+    # Позже удалим хэш, чтобы не засорять БД.
     global name
     global redis_connection
     name = hash_name
     redis_connection = r
 
     def wrapper(num: int) -> int:
+        """
+        Функция повторяет сигнатуру функции
+        multiplier, принимая int и возвращая
+        аргумент, помноженный на 2.
+        Если аргумент отсутствует в кэше,
+        декорируемая функция выполняется,
+        а результат кэшируется, в противном случае
+        результат извлекается из БД
+        и конвертируется в int.
+        """
         nonlocal r
 
         if not r.hexists(hash_name, num):
@@ -38,25 +62,31 @@ def cache_decorator(f, verbose=False):
 def multiplier(number: int):
     return number * 2
 
+# Используем более эксплицитный способ декорирования,
+# чтобы передать True в качестве значения verbose.
 multiplier = cache_decorator(multiplier, True)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
+
+    # Если подключиться к серверу Redis не удалось,
+    # завершаем работу скрипта.
+    if not multiplier:
+        exit(1)
 
     print(
         '\nУмножаем int на 2.\n'
-        'При первом вызове функция печатает ВЫЗОВ,\n'
-        'При повторном - КЭШ:\n')
+        'При первом вызове с аргументом X\n'
+        'функция печатает ВЫЗОВ, при повторном - КЭШ:\n')
     print(multiplier(1))
     print(multiplier(15))
-    print(multiplier(3000))
     print(multiplier(-89))
+    print(multiplier(3000))
     print(multiplier(1))
     print(multiplier(15))
     print(multiplier(-89))
     print(multiplier(3000))
 
     print()
-    print(name)
-    # чистим БД
-    # redis_connection.delete(name)
-    print("результат удаления: ", redis_connection.delete(name))
+    # Чистим БД.
+    redis_connection.delete(name)
